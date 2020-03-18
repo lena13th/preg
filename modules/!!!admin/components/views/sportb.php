de8ba56c8f8593428587908f70edaf0576c156db
elena <lena13th@gmail.com>
2020-03-18 20:58:14 +0500

    delete old files

--- modules/!!!admin/components/views/sportb.php
+++
@@ -1,8 +0,0 @@
-
-<!--'items' => [-->
-<?php foreach ($sportbs as $sportb): ?>
-
-['label' => '<?= $sportb->title ?>', 'icon' => 'gear', 'url' => ['sportbuilding/index'],],
-<?php endforeach; ?>
-
-<!--],-->
